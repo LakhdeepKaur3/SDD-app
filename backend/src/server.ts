61dddb4bf2785403b8a6ce86bdf7cc1c253cb6c0
Lakhdeep Kaur <lakhdeepkaur@Lakhdeeps-MacBook-Air.local>
2026-09-02 22:01:36 +0530

    Fix CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,9 +8,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://sdd-app-1037941709465.asia-south1.run.app/"
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: allowedOrigins
 }));
+
 app.use(express.json());
 
 app.use('/api/todos', todosRouter);
